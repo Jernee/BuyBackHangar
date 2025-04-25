f1eeb05354d8070c47586a58457f370297bc1bf7
Jernee <68087000+Jernee@users.noreply.github.com>
2025-04-25 08:43:45 -0400

    Fixing signals.py again

--- buyback_hangar/signals.py
+++ buyback_hangar/signals.py
@@ -1,9 +1,1 @@
-from django.dispatch import receiver
-from allianceauth.authentication.signals import user_created
-import logging
-
-logger = logging.getLogger(__name__)
-
-@receiver(user_created)
-def handle_new_user(sender, user, **kwargs):
-    logger.info(f"[BuyBackHangar] New user created: {user.username}")
+#
